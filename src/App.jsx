89273a8140202604de1a8ddccca85a0a2b01a651
thaihieu25595-crypto <thaihieu25595@gmail.com>
2026-05-28 23:24:01 +0700

    AAAAAASS

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ function App() {
   return (
     <div
       style={{
-        fontFamily: "Arial",
-        background: "#f5f5f5",
+        width: "100%",
         minHeight: "100vh",
+        background: "#f5f5f5",
+        fontFamily: "Arial",
       }}
     >
       {/* TOPBAR */}
@@ -14,34 +15,26 @@ function App() {
         style={{
           background: "#111",
           color: "#fff",
-          padding: "8px 40px",
+          padding: "8px 15px",
           display: "flex",
           justifyContent: "space-between",
           fontSize: "14px",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            gap: "25px",
-          }}
-        >
-          <div>Thái Bảo Lighting - Đèn trang trí giá tốt</div>
-          <div>Giới thiệu</div>
-          <div>Liên hệ</div>
-        </div>
+        <div>Thái Bảo Lighting - Đèn trang trí giá tốt</div>
 
-        <div>📘 🎵 ▶</div>
+        <div>Hotline: 0935 351 095</div>
       </div>
 
       {/* HEADER */}
       <div
         style={{
           background: "#fff",
-          padding: "18px 40px",
+          padding: "20px 15px",
           display: "flex",
-          justifyContent: "space-between",
           alignItems: "center",
+          justifyContent: "space-between",
+          gap: "20px",
         }}
       >
         {/* LOGO */}
@@ -52,44 +45,36 @@ function App() {
             gap: "15px",
           }}
         >
-          <div
+          <img
+            src="https://i.imgur.com/8Km9tLL.png"
+            alt="logo"
             style={{
-              width: "70px",
-              height: "70px",
-              background: "#d4a017",
-              borderRadius: "50%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              color: "#fff",
-              fontSize: "34px",
-              fontWeight: "bold",
+              width: "90px",
+              height: "90px",
+              objectFit: "contain",
             }}
-          >
-            💡
-          </div>
+          />
 
           <div>
             <div
               style={{
-                fontSize: "38px",
+                fontSize: "42px",
                 fontWeight: "bold",
-                color: "#c59b11",
+                color: "#b8860b",
                 lineHeight: 1,
               }}
             >
-              THÁI BẢO
+              THÁI BẢO LIGHTING
             </div>
 
             <div
               style={{
-                fontSize: "34px",
-                fontWeight: "bold",
-                color: "#c59b11",
-                lineHeight: 1,
+                color: "#666",
+                marginTop: "5px",
+                fontSize: "18px",
               }}
             >
-              LIGHTING
+              Chuyên cung cấp đèn trang trí
             </div>
           </div>
         </div>
@@ -98,7 +83,7 @@ function App() {
         <div
           style={{
             display: "flex",
-            width: "520px",
+            width: "500px",
             height: "48px",
           }}
         >
@@ -116,10 +101,10 @@ function App() {
           <button
             style={{
               width: "60px",
-              border: "none",
               background: "#7a3e10",
+              border: "none",
               color: "#fff",
-              fontSize: "22px",
+              fontSize: "20px",
               cursor: "pointer",
             }}
           >
@@ -127,74 +112,30 @@ function App() {
           </button>
         </div>
 
-        {/* RIGHT */}
+        {/* HOTLINE */}
         <div
           style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "30px",
+            textAlign: "right",
           }}
         >
-          {/* HOTLINE */}
           <div
             style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "12px",
+              color: "#777",
+              fontSize: "14px",
             }}
           >
-            <div
-              style={{
-                width: "50px",
-                height: "50px",
-                borderRadius: "50%",
-                background: "red",
-                color: "#fff",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                fontSize: "24px",
-              }}
-            >
-              ☎
-            </div>
-
-            <div>
-              <div
-                style={{
-                  color: "#777",
-                  fontSize: "14px",
-                }}
-              >
-                Chăm sóc khách hàng 24/7
-              </div>
-
-              <div
-                style={{
-                  fontSize: "20px",
-                  fontWeight: "bold",
-                }}
-              >
-                0935.351.095
-              </div>
-            </div>
+            Chăm sóc khách hàng
           </div>
 
-          {/* CART */}
-          <button
+          <div
             style={{
-              background: "#7a3e10",
-              color: "#fff",
-              border: "none",
-              padding: "14px 24px",
+              color: "red",
               fontWeight: "bold",
-              cursor: "pointer",
-              borderRadius: "4px",
-              fontSize: "16px",
+              fontSize: "32px",
             }}
           >
-            GIỎ HÀNG 🛒
-          </button>
+            0935 351 095
+          </div>
         </div>
       </div>
 
@@ -204,8 +145,8 @@ function App() {
           background: "#181313",
           color: "#fff",
           display: "flex",
-          gap: "28px",
-          padding: "18px 40px",
+          gap: "30px",
+          padding: "18px 15px",
           fontWeight: "bold",
           fontSize: "17px",
         }}
@@ -215,11 +156,76 @@ function App() {
         <div>ĐÈN TƯỜNG</div>
         <div>ĐÈN THẢ</div>
         <div>ĐÈN CHÙM</div>
-        <div>MÂM-ỐP TRẦN</div>
-        <div>ĐÈN BÀN</div>
-        <div>QUẠT TRẦN ĐÈN</div>
+        <div>MÂM ÁP TRẦN</div>
+        <div>QUẠT TRẦN</div>
         <div>ĐÈN NGOẠI THẤT</div>
         <div>TIN TỨC</div>
+      </div>
+
+      {/* BANNER */}
+      <div
+        style={{
+          width: "100%",
+          height: "520px",
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?q=80&w=1600&auto=format&fit=crop)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          position: "relative",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            inset: 0,
+            background: "rgba(0,0,0,0.45)",
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: "80px",
+          }}
+        >
+          <div
+            style={{
+              color: "#fff",
+              maxWidth: "600px",
+            }}
+          >
+            <div
+              style={{
+                fontSize: "72px",
+                fontWeight: "bold",
+                lineHeight: 1.1,
+              }}
+            >
+              Kiến tạo không gian sống đẹp
+            </div>
+
+            <div
+              style={{
+                fontSize: "28px",
+                marginTop: "20px",
+              }}
+            >
+              Hơn 500 mẫu đèn hiện đại và cao cấp
+            </div>
+
+            <button
+              style={{
+                marginTop: "35px",
+                background: "#c89211",
+                color: "#fff",
+                border: "none",
+                padding: "18px 45px",
+                fontSize: "18px",
+                fontWeight: "bold",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              MUA NGAY
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
